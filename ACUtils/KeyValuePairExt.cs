155eca0a2edd51a3b2c3ff97fb870c69d8c7df0a
Andrea Cattaneo <andrea.cattaneo@infoesse.it>
2019-03-01 14:30:22 +0100

    KeyValuePair: aggiunto truncate

--- ACUtils/KeyValuePairExt.cs
+++ ACUtils/KeyValuePairExt.cs
@@ -10,6 +10,16 @@ namespace ACUtils
             return new KeyValuePair<string, object>(key, value);
         }
 
+        public static KeyValuePair<string, object> WithValue(this string key, string value, int maxLenght)
+        {
+            return new KeyValuePair<string, object>(key, value.Truncate(maxLenght));
+        }
+
+        public static KeyValuePair<string, KeyValuePair<SqlDbType, object>> WithValue(this string key, object value, SqlDbType type)
+        {
+            return WithValueType(key, value, type);
+        }
+
         public static KeyValuePair<string, KeyValuePair<SqlDbType, object>> WithValueType(this string key, object value, SqlDbType type)
         {
             return new KeyValuePair<string, KeyValuePair<SqlDbType, object>>(key, new KeyValuePair<SqlDbType, object>(type, value));
