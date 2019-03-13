98ea023850d0bb6ee1f783eb817d8d767de9cdf5
Andrea Cattaneo <andrea.cattaneo@infoesse.it>
2019-03-13 10:39:04 +0100

    correzioni RegEditUtil

--- ACUtils/RegEditUtil.cs
+++ ACUtils/RegEditUtil.cs
@@ -1,17 +1,16 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.AccessControl;
 using System.Security.Principal;
-using Microsoft.Win32;
 
 namespace ACUtils
 {
     public class RegEditUtil
     {
-        readonly string _regKey;
-
-        readonly RegistryKey _baseKey = Registry.LocalMachine;
+        private readonly string _regKey;
+        private readonly RegistryKey _baseKey = Registry.LocalMachine;
 
         public RegEditUtil(RegistryKey baseKey, string regKey = @"Software\ACUtils")
         {
@@ -85,8 +84,12 @@ namespace ACUtils
 
         public void Write(string keyname, string value)
         {
-            _baseKey.SetValue($"{_regKey}\\{keyname}", value);
+            using (RegistryKey key = _baseKey.CreateSubKey(_regKey, true))
+            {
+                key.SetValue(keyname, value);
+            }
         }
+
 
         public string Read(string keyname, string subKeyname)
         {
@@ -94,7 +97,7 @@ namespace ACUtils
             {
                 using (RegistryKey subKey = _baseKey.OpenSubKey($"{_regKey}\\{keyname}", false))
                 {
-                    return Convert.ToString(subKey?.GetValue(subKeyname, null));
+                    return Read(subKey, keyname);
                 }
             }
             catch (Exception)
@@ -107,7 +110,23 @@ namespace ACUtils
         {
             try
             {
-                return Convert.ToString(_baseKey?.GetValue($"{_regKey}\\{keyname}", null));
+                using (RegistryKey subKey = _baseKey.OpenSubKey(_regKey, false))
+                {
+                    return Read(subKey, keyname);
+                }
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+
+        public string Read(RegistryKey key, string keyname)
+        {
+            try
+            {
+                return Convert.ToString(key?.GetValue(keyname, null));
             }
             catch (Exception)
             {
@@ -139,7 +158,7 @@ namespace ACUtils
         {
             try
             {
-                    return regKey?.GetValue(subKeyname, null) != null;
+                return regKey?.GetValue(subKeyname, null) != null;
             }
             catch (Exception)
             {
@@ -149,7 +168,10 @@ namespace ACUtils
 
         public List<string> List()
         {
-            return List(_baseKey);
+            using (RegistryKey subKey = _baseKey.OpenSubKey(_regKey, false))
+            {
+                return List(subKey);
+            }
         }
 
         public List<string> List(string keyname)
@@ -163,10 +185,12 @@ namespace ACUtils
 
         public List<string> List(RegistryKey regKey)
         {
+            /*
             return (
                     from k in regKey?.GetValueNames()
                     select k.Remove(0, this._regKey.Length).TrimStart('\\')
                 ).ToList();
+            */
             return new List<string>(regKey?.GetValueNames());
         }
     }
